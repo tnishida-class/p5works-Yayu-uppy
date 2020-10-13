082ae9fb199e352a4adefdecce67a6fc710f942f
Yayu-uppy <72425695+Yayu-uppy@users.noreply.github.com>
2020-10-13 11:23:00 +0900

    Update sketch.js

--- section1-1/sketch.js
+++ section1-1/sketch.js
@@ -7,10 +7,14 @@ function draw() {
   strokeWeight(4);
   fill(178, 100, 162);
   triangle(12,120,116,120,116,10);
+  triangle(12,220,116,120,116,10);
   fill(94, 185, 84);
-  triangle(0,108,106,108,106,0);
+  triangle(0,208,106,108,106,0);
   fill(255);
   textSize(32);
   textFont("serif");
-  text("46", 68, 100);
+  text("17", 68, 100);
 }
+
+// triangle(12,120,116,120,116,10);
+// triangle(0,108,106,108,106,0);
